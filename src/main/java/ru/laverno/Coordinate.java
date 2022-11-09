937fd53577fc3e1bd0a3d15e856338d654f70c31
Max <asd@yandex.ru>
2022-11-09 19:46:10 +0300

    Вторая версия MouseSaver - Добавлена смена угла при помощи аргумента. Добавлен выход из приложения при помощи нажатия любой клавиши клавиатуры.
    #Second version of MouseSaver
    ##Added change degree by argument.
    ##Now you can close application by press any key.

--- src/main/java/ru/laverno/Coordinate.java
+++ src/main/java/ru/laverno/Coordinate.java
@@ -1,61 +1,116 @@
 package ru.laverno;
 
+import lombok.Getter;
+
 import static ru.laverno.Border.*;
 
+@Getter
 public class Coordinate {
 
-    private int x;
-    private int y;
-    private boolean directionHorizontal; //true = right, false = left
-    private boolean directionVertical; //true = down, false = up
+    private double x;
+    private double y;
+    private final double xStep;
+    private final double yStep;
+    private boolean rightDirectionHorizontal;
+    private boolean downDirectionVertical;
+    private int counterChangeDirection;
 
-    public Coordinate(int startX, int startY, boolean directionHorizontal, boolean directionVertical) {
-        if (startX < UPPER.getFirstPoint()
-                || startY < RIGHT.getFirstPoint()
-                ||startX > UPPER.getSecondPoint()
-                || startY > RIGHT.getSecondPoint()) {
+    private Coordinate(CoordinateBuilder builder) {
+        if (builder.x < UPPER.getFirstPoint()
+                || builder.y < RIGHT.getFirstPoint()
+                || builder.x > UPPER.getSecondPoint()
+                || builder.y > RIGHT.getSecondPoint()) {
             throw new IllegalArgumentException();
         }
 
-        this.x = startX;
-        this.y = startY;
-        this.directionHorizontal = directionHorizontal;
-        this.directionVertical = directionVertical;
+        this.x = builder.x;
+        this.y = builder.y;
+        this.rightDirectionHorizontal = builder.rightDirectionHorizontal;
+        this.downDirectionVertical = builder.downDirectionVertical;
+        this.xStep = builder.xStep;
+        this.yStep = builder.yStep;
+        this.counterChangeDirection = -2;
     }
 
     public int calculateX() {
         checkHorizontalBorder();
 
-        if (directionHorizontal) return ++x;
-        else return --x;
+        if (rightDirectionHorizontal) return (int) (x += xStep);
+        else return (int) (x -= xStep);
     }
 
     public int calculateY() {
         checkVerticalBorder();
 
-        if (directionVertical) return ++y;
-        else return --y;
+        if (downDirectionVertical) return (int) (y += yStep);
+        else return (int) (y -= yStep);
     }
 
     private void checkHorizontalBorder() {
-        if (x == UPPER.getSecondPoint()) {
-            directionHorizontal = false;
+        if (x >= UPPER.getSecondPoint()) {
+            rightDirectionHorizontal = false;
+            counterChangeDirection++;
         }
-        if (x == LOWER.getSecondPoint()) {
-            directionHorizontal = true;
+        if (x <= LOWER.getSecondPoint()) {
+            rightDirectionHorizontal = true;
+            counterChangeDirection++;
         }
     }
 
     private void checkVerticalBorder() {
-        if (y == LEFT.getSecondPoint()) {
-            directionVertical = true;
+        if (y <= LEFT.getSecondPoint()) {
+            downDirectionVertical = true;
+            counterChangeDirection++;
         }
-        if (y == RIGHT.getSecondPoint()) {
-            directionVertical = false;
+        if (y >= RIGHT.getSecondPoint()) {
+            downDirectionVertical = false;
+            counterChangeDirection++;
         }
     }
 
     public boolean isFinish() {
-        return !(x == LOWER.getFirstPoint() && y == RIGHT.getSecondPoint());
+        return x == LOWER.getFirstPoint() && y == RIGHT.getSecondPoint();
     }
+
+    public static class CoordinateBuilder {
+
+        private double x;
+        private double y;
+        private final double xStep;
+        private final double yStep;
+        private boolean rightDirectionHorizontal;
+        private boolean downDirectionVertical;
+
+        public CoordinateBuilder(final double xStep, final double yStep) {
+            this.xStep = xStep;
+            this.yStep = yStep;
+            this.rightDirectionHorizontal = true;
+            this.downDirectionVertical = true;
+        }
+
+        public CoordinateBuilder setX(final double x) {
+            this.x = x;
+            return this;
+        }
+
+        public CoordinateBuilder setY(final double y) {
+            this.y = y;
+            return this;
+        }
+
+        public CoordinateBuilder setRightDirectionHorizontal(final boolean rightDirectionHorizontal) {
+            this.rightDirectionHorizontal = rightDirectionHorizontal;
+            return this;
+        }
+
+        public CoordinateBuilder setDownDirectionVertical(final boolean downDirectionVertical) {
+            this.downDirectionVertical = downDirectionVertical;
+            return this;
+        }
+
+        public Coordinate build() {
+            return new Coordinate(this);
+        }
+    }
+
 }
